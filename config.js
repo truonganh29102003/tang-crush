586fd0f1184b9cd9b01e35e4f14bb82a292f2770
TienLoc03 <89780288+TienLoc03@users.noreply.github.com>
2021-08-30 20:03:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
     introTitle: 'Bae à!',
-    introDesc: `Con chó nó giống con gà
-    Quả cà tím nó giống bí ngô
-    Như cái cách mà anh thích nàng
-    Thích là thích chứ chẳng lý do`,
+    introDesc: `Em là hoa mà chưa có chậu
+    Anh là chậu mà chẳng có hoa
+    Thế vì sao mình không chung ngõ
+    Đến cả khổ mà vẫn có qua`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    btnYes: 'YÊU mình Anh thoiii <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Gọi Anh là mặt trời Vì yêu Anh chính là điều sáng suốt <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Tối nay 7h anh qua đụ nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://m.me/n.t.loc.203' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
